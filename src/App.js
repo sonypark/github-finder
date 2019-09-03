e65747dad26975380992f970c38f1176913d8e18
Sony <sonypark0204@gmail.com>
2019-09-03 12:00:40 +0900

    [feat] Create Navbar component
    - Navar 컴포넌트 생성
    - title, icon 속성값(props)을 디폴트값으로 선언

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <Navbar title='Github Finder'/>
+                <Navbar/>
             </div>
         );
     }
