eea711732597a64bdf37b5cdfd5c63dc5f025145
Sony <sonypark0204@gmail.com>
2019-09-03 15:29:48 +0900

    [feat] Add spinner when loading users
    - 유저 리스트 로딩 시 스피너 추가
    - 리팩토링

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import React, {Component} from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users';
+import axios from 'axios';
 
 
 class App extends Component {
+    state = {
+        users: [],
+        loading: true
+    };
+
+    async componentDidMount() {
+        this.setState({loading: true});
+
+        const res = await axios.get('https://api.github.com/users');
+
+        this.setState({users: res.data , loading: false});
+        console.log(res);
+    }
+
     render() {
         return (
             <div className="App">
                 <Navbar/>
                 <div className="container">
-                    <Users/>
+                    <Users loading={this.state.loading} users={this.state.users} />
                 </div>
             </div>
         );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,45 +1,25 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
 
-class Users extends Component {
-    state = {
-        users: [
-            {
-                id: "1",
-                login: "mojombo",
-                avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-                html_url: "https://github.com/mojombo"
-            },
-            {
-                id: "2",
-                login: "defunkt",
-                avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
-                html_url: "https://github.com/defunkt"
-            },
-            {
-                id: "3",
-                login: "pjhyett",
-                avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
-                html_url: "https://github.com/pjhyett"
-            }
-        ]
-    };
+const getUserCmps = (users) => {
+    return users.map(user => <UserItem key={user.id} user={user}/>)
+};
 
-    getUserCmps(users) {
-        return users.map(user => <UserItem key={user.id} user={user}/>)
-    }
+const Users = ({users, loading}) => {
 
-    render() {
-        const {users} = this.state;
-        const userCmps = this.getUserCmps(users);
+    if(loading){
+        return <Spinner/>
+    }else {
+        const userCmps = getUserCmps(users);
         return (
             <div style={userStyle}>
                 {userCmps}
             </div>
         );
     }
-}
+};
 
 const userStyle = {
     display: 'grid',
@@ -47,6 +27,9 @@ const userStyle = {
     gridGap: '1rem'
 };
 
-Users.propTypes = {};
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired,
+};
 
 export default Users;
