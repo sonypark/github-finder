70207ba5df2d75f8a869868b8d41424ffa534e21
Sony <sonypark0204@gmail.com>
2019-09-03 20:43:36 +0900

    [feat] Set up react router
    - `a tag` VS `Link`
    - `a` 태그를 사용하면 **state**가 **초기화** 된다.
    - `react-router-dom`의 `Link`를 사용하면 **state 값이 유지된다.**
        - 따라서 다른 링크를 누른 후 다시 돌아와도 그 상태를 그대로 유지하고 있다.
        - ex) Github-Finder에서 검색창에 user를 검색한 후 다른 화면에 갔다가 돌아오는 경우
            - `a` 태그라면 검색한 유저 목록이 초기화 되어 빈화면이 뜬다.
            - `Link` 태그라면 검색한 유저 목록이 그대로 유지된다.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import env from 'dotenv';
 // env.config();
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+import About from './components/pages/About'
 import axios from 'axios';
 
 
@@ -19,18 +21,14 @@ class App extends Component {
     // Get default users
     async componentDidMount() {
         this.setState({loading: true});
-
         const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
         this.setState({users: res.data, loading: false});
     }
 
     // Search Github users
     searchUsers = async text => {
         this.setState({loading: true});
-
         const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
         this.setState({users: res.data.items, loading: false});
     };
 
@@ -40,27 +38,35 @@ class App extends Component {
     // Set Alert
     setAlert = (msg, type) => {
         this.setState({alert: {msg, type}});
-
-        setTimeout(()=> this.setState({alert:null}), 3000);
+        setTimeout(() => this.setState({alert: null}), 3000);
     };
 
     render() {
         const {users, loading} = this.state;
 
         return (
-            <div className="App">
-                <Navbar/>
-                <div className="container">
-                    <Alert alert={this.state.alert}/>
-                    <Search
-                        searchUsers={this.searchUsers}
-                        clearUsers={this.clearUsers}
-                        showClear={users.length > 0}
-                        setAlert={this.setAlert}
-                    />
-                    <Users loading={loading} users={users}/>
+            <Router>
+                <div className="App">
+                    <Navbar/>
+                    <div className="container">
+                        <Alert alert={this.state.alert}/>
+                        <Switch>
+                            <Route exact path='/' render={props => (
+                                <Fragment>
+                                    <Search
+                                        searchUsers={this.searchUsers}
+                                        clearUsers={this.clearUsers}
+                                        showClear={users.length > 0}
+                                        setAlert={this.setAlert}
+                                    />
+                                    <Users loading={loading} users={users}/>
+                                </Fragment>
+                            )}/>
+                            <Route exact path='/about' component={About}/>
+                        </Switch>
+                    </div>
                 </div>
-            </div>
+            </Router>
         );
     }
 }
