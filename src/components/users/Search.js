c9577d0d798ef9634b6a7c7f5c80b9e1c41b416c
Sony <sonypark0204@gmail.com>
2019-09-03 17:46:17 +0900

    [feat] Clear users from state
    - 유저 목록 clear 하는 버튼 추가
    - clear 버튼을 누르면 state 에서 users를 빈 배열로 만든다

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -8,6 +8,8 @@ class Search extends Component {
 
     static propTypes = {
         searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired,
     };
 
     onSubmit = e => {
@@ -16,9 +18,11 @@ class Search extends Component {
         this.setState({text: ''});
     };
 
-    onChange = e => this.setState({ [e.target.name] : e.target.value } );
+    onChange = e => this.setState({[e.target.name]: e.target.value});
 
     render() {
+        const {showClear, clearUsers} = this.props;
+
         return (
             <div>
                 <form onSubmit={this.onSubmit} className="form">
@@ -31,6 +35,9 @@ class Search extends Component {
                     />
                     <input type="submit" value="Search" className='btn btn-dark btn-block'/>
                 </form>
+                {showClear && (
+                    <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>
+                )}
             </div>
         );
     }
