1c16bd532854cea5493f065486934f28269ebb87
Sony <sonypark0204@gmail.com>
2019-09-04 11:28:20 +0900

    [feat] Create single user component
    - User 컴포넌트 생성
    - 특정 유저의 상세 페이지를 보기위한 컴포넌트

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About'
@@ -14,6 +15,7 @@ import axios from 'axios';
 class App extends Component {
     state = {
         users: [],
+        user: {},
         loading: false,
         alert: null
     };
@@ -32,6 +34,12 @@ class App extends Component {
         this.setState({users: res.data.items, loading: false});
     };
 
+    getUser = async username => {
+        this.setState({loading: true});
+        const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+        this.setState({user: res.data, loading: false});
+    };
+
     // Clear users
     clearUsers = () => this.setState({users: [], loading: false});
 
@@ -42,7 +50,7 @@ class App extends Component {
     };
 
     render() {
-        const {users, loading} = this.state;
+        const {users, user, loading} = this.state;
 
         return (
             <Router>
@@ -63,6 +71,9 @@ class App extends Component {
                                 </Fragment>
                             )}/>
                             <Route exact path='/about' component={About}/>
+                            <Route exact path='/user/:login' render={props => (
+                                <User {...props} getUser={this.getUser} user={user} loading={loading}/>
+                            )} />
                         </Switch>
                     </div>
                 </div>
