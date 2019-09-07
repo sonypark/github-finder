099461aa5aa3b19a7ccfc7711d7cfd72cdc42c55
Sony <sonypark0204@gmail.com>
2019-09-07 12:04:09 +0900

    [refactor] Create home page component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React, {Fragment, useEffect, useState} from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About'
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
+import Home from './components/pages/Home';
 import axios from 'axios';
 
 
@@ -37,12 +36,7 @@ const App = () => {
                         <div className="container">
                             <Alert/>
                             <Switch>
-                                <Route exact path='/' render={props => (
-                                    <Fragment>
-                                        <Search/>
-                                        <Users/>
-                                    </Fragment>
-                                )}/>
+                                <Route exact path='/' component={Home} />
                                 <Route exact path='/about' component={About}/>
                                 <Route exact path='/user/:login' component={User}/>
                             </Switch>
